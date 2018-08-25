9d34a46f0f31684a4b54934f54d8ec7c35b34926
m2paulc <hyp3rsp8c3@gmail.com>
2018-08-25 16:43:40 -0400

    fixed navigation functionality

--- js/index.js
+++ js/index.js
@@ -2,14 +2,17 @@ window.onload = function() {
     const navigation = document.getElementsByClassName('navigation');
     const navCheckbox = document.getElementById('navigation-toggle');
     const navItems = document.querySelectorAll('.navigation__item');
+
+    let scrollHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
     
     navItems.forEach(item => {
         item.addEventListener('click', () => { 
-            // let linkItem = item.querySelectorAll('.navigation__link').length.getAttribute('a');
+            let linkItem = item.getElementsByTagName('a')[0].getAttribute('href');
             navCheckbox.checked = false;
-            // console.log(linkItem);
-            window.scrollTo(0, document.getElementById('features-section').offsetHeight);
-            console.log(document.getElementById('features-section'));
         });
     });
 }
